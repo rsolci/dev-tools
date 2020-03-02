3cd933dfd8f2cb40fa8e6c50c2467e735553a382
Rafael Solci <rsolci@gmail.com>
2020-03-02 21:47:47 +0100

    Adding tailwind

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,7 +4,7 @@ const Sidebar = ({children}) => {
   const [open, setOpen] = useState(true)
   return (
     <nav aria-hidden={!open}>
-      <header>
+      <header className='max-w-sm rounded overflow-hidden shadow-lg font-bold text-purple-500 text-xl mb-2'>
         Nav Header
       </header>
       <div>
