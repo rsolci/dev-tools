aaf6a648c3ea2559c39fa16c5a38064ee2053ae9
Rafael Solci <rsolci@gmail.com>
2020-03-03 11:22:00 +0100

    Very simple QR generation

--- src/components/pages/QrGeneration.js
+++ src/components/pages/QrGeneration.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import QRCode from 'qrcode'
+
+const generateDataUrl = async text => {
+  return await QRCode.toDataURL(text)
+}
 
 const QrGeneration = () => {
+  const [textToEncode, setTextToEncode] = useState('')
+  const [imageData, setImageData] = useState('')
+
+  const generate = async () => {
+    const dataUrl = await generateDataUrl(textToEncode)
+    console.log(dataUrl)
+    setImageData(dataUrl)
+  }
+
   return (
     <div>
       Qr Generation
+      <input type='text' onChange={(e) => setTextToEncode(e.target.value)} />
+      <button onClick={generate}>Generate</button>
+      <img src={imageData} />
       <p>
         The word "QR Code" is registered trademark of:
         DENSO WAVE INCORPORATED
