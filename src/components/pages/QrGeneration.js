db429d25e467d4a5e8be2249a32f5e99f8c3f0c7
Rafael Solci <rsolci@gmail.com>
2020-03-03 13:48:29 +0100

    Generating the image on the go

--- src/components/pages/QrGeneration.js
+++ src/components/pages/QrGeneration.js
@@ -1,26 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useCallback } from 'react'
+
+import debounce from 'lodash/debounce'
+
 import QRCode from 'qrcode'
 
 const generateDataUrl = async text => {
+  if (!text) return;
   return await QRCode.toDataURL(text)
 }
-
 const QrGeneration = () => {
   const [textToEncode, setTextToEncode] = useState('')
   const [imageData, setImageData] = useState('')
 
-  const generate = async () => {
-    const dataUrl = await generateDataUrl(textToEncode)
-    console.log(dataUrl)
-    setImageData(dataUrl)
+  const delayedGenerate = useCallback(debounce(async text => {
+    setImageData(await generateDataUrl(text))
+  }, 200), [])
+
+  const instantGenerate = async () => {
+    setImageData(await delayedGenerate(textToEncode))
+  }
+
+  const inputChanged = async (event) => {
+    setTextToEncode(event.target.value)
+    delayedGenerate(event.target.value)
   }
 
   return (
     <div>
       Qr Generation
-      <input type='text' onChange={(e) => setTextToEncode(e.target.value)} />
-      <button onClick={generate}>Generate</button>
-      <img src={imageData} alt={`QR Image for ${textToEncode}`} />
+      <input type='text' onChange={inputChanged} value={textToEncode} />
+      <button onClick={instantGenerate}>Generate</button>
+      <img src={imageData} alt={`QR for ${textToEncode}`} />
       <p>
         The word "QR Code" is registered trademark of:
         DENSO WAVE INCORPORATED
