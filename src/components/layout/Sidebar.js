65fb766440b7bb910f23014e8f394f75a3dc21a4
Rafael Solci <rsolci@gmail.com>
2020-03-04 11:10:23 +0100

    Cleaning a little for deploy

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Sidebar = ({children}) => {
   return (
     <nav className='w-56 min-w-56'>
-      <header className='max-w-sm rounded overflow-hidden shadow-lg font-bold text-purple-500 text-xl mb-2'>
-        Nav Header
+      <header >
+        Available tools
       </header>
       <div>
         {children}
