00de65ef8baf68c77a0a24d37da758f5183f8e28
Rafael Solci <rsolci@gmail.com>
2020-03-04 11:11:38 +0100

    Updates

---
+++ precache-manifest.858982454024f937e16358d401a98a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611903835dc7b345f66d97245a832bc9",
+    "url": "/dev-tools/index.html"
+  },
+  {
+    "revision": "0290e32666c5fad0c24f",
+    "url": "/dev-tools/static/css/main.ec75cc2c.chunk.css"
+  },
+  {
+    "revision": "4c40732760b62c7d7fc7",
+    "url": "/dev-tools/static/js/2.39c44d88.chunk.js"
+  },
+  {
+    "revision": "3e7eb0c51ce474724fe2295510ac9693",
+    "url": "/dev-tools/static/js/2.39c44d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0290e32666c5fad0c24f",
+    "url": "/dev-tools/static/js/main.a3c43ab4.chunk.js"
+  },
+  {
+    "revision": "f81a0bdd64174558d369",
+    "url": "/dev-tools/static/js/runtime-main.8e6c7ce9.js"
+  }
+]);
